620acba760d3157c777ca8f9fc4ee52eb3a27426
SBizzle923 <sbizzle923@gmail.com>
2025-06-07 11:25:36 -0400

    Ch2: L3: FPS
    dt timer and clock

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    pygame.display.set_caption("Boot Asteroids Game")
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,10 +18,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()

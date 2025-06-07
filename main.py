443e465b0c5f7745c63039409405f583ce70997c
SBizzle923 <sbizzle923@gmail.com>
2025-06-07 11:59:24 -0400

    Ch2: L8: Groups

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -23,8 +28,11 @@ def main():
                 return
             
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for thing in updatable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0
